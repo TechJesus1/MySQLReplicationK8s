c7fc80e0bbccd4d69ae9e24db586042abc45c1d6
TechJesus1 <164140015+TechJesus1@users.noreply.github.com>
2024-06-04 13:00:25 +0530

    Remove unused files

--- run-my-sql-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f configmap.yaml
-kubectl apply -f services.yaml
-kubectl apply -f statefulset.yaml
-
-echo "Now watching progress of Pods"
-
-kubectl get pods -l app=mysql --watch
